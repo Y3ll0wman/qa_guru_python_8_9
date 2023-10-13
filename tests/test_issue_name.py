e90d9e8c364b2a96f6d8ef0f258c6ce7e4975d35
Konstantin Varvarkin <varvarkin.k@gmail.com>
2023-10-13 18:57:39 +0300

    Добавил тест с allure.steps

--- tests/test_issue_name.py
+++ tests/test_issue_name.py
@@ -10,4 +10,21 @@ def test_issue_name_selene():
     browser.element('#issues-tab').should(be.visible).click()
     browser.element(by.link_text('Issue_created_to_test_allure_reports')).should(be.visible).click()
     browser.element('.gh-header-title').should(have.text('Issue_created_to_test_allure_reports'))
-    print()
+
+
+def test_issue_name_allure_steps():
+    with allure.step("Открыть github.com"):
+        browser.open('https://github.com/')
+    with allure.step("Нажать на поисковую строку"):
+        browser.element('.search-input').should(be.visible).click()
+    with allure.step("Ввести 'eroshenkoam/allure-example' и нажать Enter"):
+        browser.element('#query-builder-test').should(be.visible).type('eroshenkoam/allure-example').press_enter()
+    with allure.step("Нажать на ссылку содержащую текст 'eroshenkoam/allure-example'"):
+        browser.element(by.link_text('eroshenkoam/allure-example')).should(be.visible).click()
+    with allure.step("Кликнуть на Issues-tab"):
+        browser.element('#issues-tab').should(be.visible).click()
+    with allure.step("Нажать на ссылку содержащую текст 'Issue_created_to_test_allure_reports'"):
+        browser.element(by.link_text('Issue_created_to_test_allure_reports')).should(be.visible).click()
+    with allure.step("Проверить, что заголовок содержит текст 'Issue_created_to_test_allure_reports'"):
+        browser.element('.gh-header-title').should(have.text('Issue_created_to_test_allure_reports'))
+
