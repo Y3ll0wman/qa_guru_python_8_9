9f753c082868bbfae833750fa83eaffc57e21a61
Konstantin Varvarkin <varvarkin.k@gmail.com>
2023-10-14 03:42:17 +0300

    Обновил конфиг драйвера и зависимости

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,21 @@
 import pytest
 
-from selene.support.shared import browser
 from selenium import webdriver
+from selene import browser
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     options = webdriver.ChromeOptions()
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-gpu')
-    browser.config.base_url = 'https://github.com/'
-    browser.config.window_width = 1310
-    browser.config.window_height = 1410
-    browser.config.driver_name = 'chrome'
+    # options.add_argument('--no-sandbox')
+    # options.add_argument('--disable-dev-shm-usage')
+    # options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
+    options.base_url = 'https://github.com/'
+    options.window_width = 1310
+    options.window_height = 1410
+    options.driver_name = 'chrome'
+    options.browser_version = '2.37'
 
     yield
 
