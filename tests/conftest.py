2e90c6643f2db844ddc404f93c3d0850261263ff
Konstantin Varvarkin <varvarkin.k@gmail.com>
2023-10-14 13:38:11 +0300

    Обновил опции драйвера

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 
-from selene.support.shared import config
+# from selene.support.shared import config
 from selenium import webdriver
+from selene import browser
 
 
 @pytest.fixture(scope='function', autouse=True)
@@ -12,8 +13,13 @@ def browser_management():
     options.add_argument('--disable-gpu')
     options.add_argument('--headless')
 
-    config.driver_options = options
-    config.base_url = 'https://github.com/'
-    config.window_width = 1310
-    config.window_height = 1410
-    config.timeout = 15
+    # config.driver_options = options
+    # config.base_url = 'https://github.com/'
+    # config.window_width = 1310
+    # config.window_height = 1410
+    # config.timeout = 15
+    browser.config.driver_options = options
+    browser.config.base_url = 'https://github.com/'
+    browser.config.window_width = 1310
+    browser.config.window_height = 1410
+    browser.config.timeout = 15
