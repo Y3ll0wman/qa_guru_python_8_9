18332846de3fb06f5384cbc4dbee71bcd4e58d62
Konstantin Varvarkin <varvarkin.k@gmail.com>
2023-10-14 03:04:27 +0300

    Обновил путь к browser

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from selene import browser
+from selene.support.shared import browser
 
 
 @pytest.fixture(scope='function', autouse=True)
@@ -8,6 +8,7 @@ def browser_management():
     browser.config.window_width = 1310
     browser.config.window_height = 1410
     browser.config.driver_name = 'chrome'
+
     yield
 
     browser.quit()
